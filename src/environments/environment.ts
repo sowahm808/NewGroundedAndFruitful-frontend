3f9668171eb1d9ac577895ebb37eadfb666a4a8c
Michael Sowah <sowahm@gmail.com>
2026-08-08 18:12:23 -0500

    updated

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,13 @@
 export const environment = {
   production: false,
   useEmulators: true,
-  firebase: { apiKey: '', authDomain: 'localhost', projectId: 'grounded-fruitful-dev', appId: '' },
-  appCheckSiteKey: '',
+  firebase: { 
+              apiKey: 'AIzaSyAKfBL4hMl2lWK5i-uDT0Q5aUVl5QzSl-I',
+              authDomain: 'grounded-fruitful.firebaseapp.com', 
+              projectId: 'grounded-fruitful',
+              storageBucket: 'grounded-fruitful.firebasestorage.app',
+              messagingSenderId: '75338540922', 
+              appId: '1:75338540922:web:d686c5154e64d9883658c3', 
+            },
+              appCheckSiteKey: '',
 } as const;
